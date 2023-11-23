ed48c63e83562e21ded05a33b8d567edffe48316
LetTTGACO <1225751694@qq.com>
2023-11-23 13:16:27 +0800

    feat: elog配置

--- elog.config.shorturl.js
+++ elog.config.shorturl.js
@@ -1,12 +1,20 @@
 module.exports = {
   write: {
-    platform: 'yuque',
+    platform: 'yuque-pwd',
+    // Token 模式（需要语雀超级会员）
     yuque: {
       token: process.env.YUQUE_TOKEN,
       login: process.env.YUQUE_LOGIN,
       repo: process.env.YUQUE_REPO,
       onlyPublic: false,
       onlyPublished: true,
+    },
+    // 账号密码模式
+    "yuque-pwd": {
+      username: process.env.YUQUE_USERNAME,
+      password: process.env.YUQUE_PASSWORD,
+      login: process.env.YUQUE_LOGIN,
+      repo: process.env.YUQUE_REPO,
     }
   },
   deploy: {
